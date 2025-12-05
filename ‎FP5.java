bb02ec4abc7ff838fac588d8ff68064d26d4a60c
univivseh <darya.khalmatoba0303@gmail.com>
2025-12-05 15:38:00 +0700

    Добавили пример работы функции replan с копией параметра plan

---
+++ ‎FP5.java
@@ -0,0 +1,31 @@
+import java.util.List;
+import java.util.ArrayList;
+class Kurs {
+	static List<String> replan (List <String> plan, String newChapt, String beforeChapt) {
+		int newChaptIndex = plan.indexOf(beforeChapt);
+		plan.add(newChaptIndex, newChapt);
+		return plan;
+
+	}
+}
+
+class FP5 {
+	public static void main (String[] args) {
+		List<String> planA = new ArrayList<>();
+		planA.add("ВВЕДЕНИЕ");
+		planA.add("ГЛАВА 1");
+		planA.add("Создание видеоигр");
+		planA.add("ГЛАВА 2");
+		List<String> planB = new ArrayList<>();
+		planB = Kurs.replan(planA, "Анализ целевой аудитории", "ГЛАВА 2");
+	}
+}
+
+class  Kurs2 {
+        static List<String> replan (List <String> plan, String newChapt, String beforeChapt) {
+                int newChaptIndex = plan.indexOf(beforeChapt);
+                List<String> replanned = new ArrayList<>(plan);
+                replaned.add(newChaptIndex, newChapt);
+                return replanned;
+        }
+}
