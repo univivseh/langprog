a281e3ad35eb2812e344f7a9645ef98beda52ad2
univivseh <167408570+univivseh@users.noreply.github.com>
2025-12-05 17:50:24 +0700

    Добавлен пример тестирования чистой функции

---
+++ FP4.java
@@ -0,0 +1,45 @@
+import java.util.List;
+import java.util.ArrayList;
+class TCalc {
+    private List<String> names = new ArrayList<>();
+    private int tPercentage = 0;
+
+    public List<String> addPerson(List<String> names, String name) {
+	    List<String> updated = new ArrayList<>(names);
+	    updated.add(name);
+	    return updated;
+    }
+   /* 
+    public int getDiscountPercentage(List<String> items1) {
+	    if(items1.contains("Книга")) 
+		    return 5;
+	    else 
+		    return 0;
+    }
+    */
+
+    public List<String> getNames() {
+        return names;
+    }
+
+    public int getTPercentage(List<String> names) {
+	if(names.size() > 5) {
+		return 20;
+	}
+	else if (names.size() > 0) {
+		return 10;
+	}
+    	return 0;
+    }	    
+}
+class FP4 {
+	public static void main(String[] args) {
+		TCalc list1 = new TCalc();
+		List<String> l1 = new ArrayList<>();
+		l1 = list1.addPerson(l1, "Ринат");
+		System.out.println("Размер чаевых для l1: " + list1.getTPercentage(l1));
+		List<String> l2 = l1;
+		l2 = list1.addPerson(l1, "Юрий");
+		System.out.println("Размер чаевых для l1: " + list1.getTPercentage(l2));
+	}
+}
